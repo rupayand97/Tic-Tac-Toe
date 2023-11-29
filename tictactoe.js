2bb3d210edea7ef46845a1350608b23e09a6fd00
Rupayan Das <hrupayandas@gmail.com>
2023-11-30 01:11:11 +0530

    added winner party gif and popup for play again

--- tictactoe.js
+++ tictactoe.js
@@ -32,35 +32,68 @@ function checkWinner()
     if(points[0]==points[1] && points[1]==points[2] && points[0]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[3]==points[4] && points[4]==points[5] && points[3]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[6]==points[7] && points[7]==points[8] && points[6]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[0]==points[3] && points[3]==points[6] && points[0]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[1]==points[4] && points[4]==points[7] && points[1]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[2]==points[5] && points[5]==points[8] && points[2]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[0]==points[4] && points[4]==points[8] && points[0]!="") 
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
     else if(points[2]==points[4] && points[4]==points[6] && points[2]!="")
     {
         h1.innerText=`player ${cp} won the game`;
+        makeParty();
     }
 
     console.log(h1);
+}
+
+function makeParty()
+{
+    let body = document.body;
+    body.style.background = `url(https://www.gifimages.pics/images/quotes/english/general/gif-for-confetti-multi-colored-52650-166764.gif)`;
+    body.style.backgroundRepeat=`no-repeat`;
+    body.style.backgroundSize=`cover`;
+
+    setTimeout(()=>{
+        let result = confirm("wanna play the game again");
+
+        if(result)
+        {
+            window.location.reload();
+        }
+        else
+        {
+            let btns = document.getElementsByClassName("cls");
+            for(let i=0; i < btns.length; i++)
+            {
+                   btns[i].setAttribute("disabled", "true")
+            }
+        }
+    }, 2000)
 }
